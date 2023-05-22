dc468c2ab52e2bd2e3204772e999310869fe4f36
marwie <marcel@gaisterhand.de>
2023-05-22 12:57:43 +0200

    fix index html, vite and package for stackblitz

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import viteCompression from 'vite-plugin-compression';
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig(async ({ command }) => {
     const { needlePlugins, useGzip, loadConfig } = await import("@needle-tools/engine/plugins/vite/index.js");
@@ -8,7 +7,6 @@ export default defineConfig(async ({ command }) => {
     return {
         base: "./",
         plugins: [
-            basicSsl(),
             useGzip(needleConfig) ? viteCompression({ deleteOriginFile: true }) : null,
             needlePlugins(command, needleConfig),
         ],
