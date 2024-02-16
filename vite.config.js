cfc7cbc6d3d326d732ea50d53d6ee8ba3347194e
Marcel Wiessler <marcel@gaisterhand.de>
2024-02-17 03:56:37 +0800

    add vite pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,32 @@
 import { defineConfig } from 'vite';
 import viteCompression from 'vite-plugin-compression';
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import { VitePWA } from 'vite-plugin-pwa';
+import { readFileSync } from "fs";
 
 export default defineConfig(async ({ command }) => {
-    const { needlePlugins, useGzip, loadConfig } = await import("@needle-tools/engine/plugins/vite/index.js");
+    const { needlePlugins, useGzip, loadConfig, getOutputDirectory } = await import("@needle-tools/engine/plugins/vite/index.js");
     const needleConfig = await loadConfig();
+
     return {
         base: "./",
         plugins: [
+            basicSsl(),
             useGzip(needleConfig) ? viteCompression({ deleteOriginFile: true }) : null,
-            needlePlugins(command, needleConfig, { noPoster: true }),
+            needlePlugins(command, needleConfig, {}),
+            VitePWA({
+                registerType: 'autoUpdate',
+                outDir: getOutputDirectory(),
+                manifest: JSON.parse(readFileSync("manifest.webmanifest", "utf8")),
+            }),
         ],
         server: {
-            https: false,
+            https: true,
             proxy: { // workaround: specifying a proxy skips HTTP2 which is currently problematic in Vite since it causes session memory timeouts.
-                'https://localhost:1107': 'https://localhost:1107'
+                'https://localhost:3000': 'https://localhost:3000'
             },
             strictPort: true,
-            port: 1107,
+            port: 3000,
         },
         build: {
             outDir: "./dist",
