12de156b0ef15fadbe866297ad90bf9d6ed3593d
Marcel Wiessler <marcel@gaisterhand.de>
2024-02-17 04:31:50 +0800

    pass in the PWA options object into needlePlugins

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,22 @@ import { defineConfig } from 'vite';
 import viteCompression from 'vite-plugin-compression';
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import { VitePWA } from 'vite-plugin-pwa';
-import { readFileSync } from "fs";
 
 export default defineConfig(async ({ command }) => {
     const { needlePlugins, useGzip, loadConfig, getOutputDirectory } = await import("@needle-tools/engine/plugins/vite/index.js");
     const needleConfig = await loadConfig();
+
+
+    /** @type {import("vite-plugin-pwa").VitePWAOptions} */
+    const PWAOptions = {};
 
     return {
         base: "./",
         plugins: [
             basicSsl(),
             useGzip(needleConfig) ? viteCompression({ deleteOriginFile: true }) : null,
-            needlePlugins(command, needleConfig, {}),
-            VitePWA({
-                registerType: 'autoUpdate',
-                outDir: getOutputDirectory(),
-                manifest: JSON.parse(readFileSync("manifest.webmanifest", "utf8")),
-            }),
+            needlePlugins(command, needleConfig, { pwaOptions: PWAOptions }),
+            VitePWA(PWAOptions),
         ],
         server: {
             https: true,
