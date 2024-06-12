151ba6c439e6dc527f96f3ad44468864411a5090
marwie <marcel@gaisterhand.de>
2024-06-12 11:32:17 +0200

    update vite template

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,29 @@
-import { Behaviour, GameObject, showBalloonMessage, registerType, NeedleEngine, DragControls, onStart, Context } from "@needle-tools/engine";
-import { AxesHelper, GridHelper } from "three";
+import { Behaviour, showBalloonMessage, DragControls, onStart, DragMode, PointerEventData, serializable, RemoteSkybox, WebXR, addComponent, ContactShadows, SceneSwitcher, findObjectOfType, OrbitControls } from "@needle-tools/engine";
 import * as THREE from "three";
 
-@registerType
+// Simple example component that does nothing but rotate an object.
 export class Rotate extends Behaviour {
+
+    @serializable()
+    speed: number = .5;
+
     start() {
         console.log(this);
         showBalloonMessage("Hello Cube");
     }
     update(): void {
-        this.gameObject.rotateY(this.context.time.deltaTime);
+        this.gameObject.rotateY(this.context.time.deltaTime * this.speed);
+    }
+    onPointerEnter(_args: PointerEventData) {
+        showBalloonMessage("Hovering Cube!");
+        this.speed *= 4;
+    }
+    onPointerExit(_args: PointerEventData) {
+        showBalloonMessage("Bye Cube!");
+        this.speed *= .25;
+    }
+    onPointerClick(_args: PointerEventData) {
+        this.gameObject.scale.multiplyScalar(1.1);
     }
 }
 
@@ -19,22 +33,63 @@ export class Rotate extends Behaviour {
 onStart(context =>{
     const scene = context.scene;
 
-    const grid = new GridHelper();
-    scene.add(grid);
-
-    const axis = new AxesHelper();
-    axis.position.y = 1;
-    scene.add(axis);
-
+    // you can use regular threejs syntax to create objects
     const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
     const material = new THREE.MeshStandardMaterial( {color: 0xdddddd} ); 
     const cube = new THREE.Mesh(geometry, material); 
+    cube.position.x = 1;
     cube.position.y += .5;
     scene.add(cube);
+    // use `addComponent` to add components to objects
+    addComponent(cube, new Rotate(), { 
+        // You can initialize component properties inline:
+        // speed: 5
+    });
 
-    const dragControls = GameObject.addNewComponent(cube, DragControls);
-    dragControls.showGizmo = false;
-    dragControls.useViewAngle = false;
+    // DragControls is a builtin Needle Engine componen to allow users to drag an object.
+    // This works on desktop as well as AR or VR
+    addComponent(cube, DragControls, {
+        showGizmo: false,
+        dragMode: DragMode.XZPlane,
+    });
 
-    GameObject.addComponent(cube, new Rotate());
+    // add WebXR support
+    addComponent(scene, WebXR, {
+        createARButton: true,
+        createQRCode: true,
+        createVRButton: true,
+        createSendToQuestButton: true,
+    });
+
+    // We can modify the background or scene lighting easily using a RemoteSkybox
+    // We can also set the skybox directly on the scene if we load it manually
+    // Or just assign a skybox-image or environment-image attribute on <needle-engine>
+    // See https://engine.needle.tools/docs/reference/needle-engine-attributes.html 
+    addComponent(scene, RemoteSkybox, {
+        url: "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/1k/studio_small_08_1k.exr"
+    });
+    // Make the background blurry
+    if(context.mainCameraComponent)
+        context.mainCameraComponent.backgroundBlurriness = 1;
+     // Let's also add a Contact Shadow component
+     ContactShadows.auto();
+
+    // To load or switch additional content it's easy to use a SceneSwitcher 
+    const sceneSwitcher = addComponent(scene, SceneSwitcher, {
+        autoLoadFirstScene: false
+    });
+    sceneSwitcher.addScene("https://engine.needle.tools/demos/gltf-progressive/assets/cyberpunk/model.glb")
+
+    sceneSwitcher.select(0).then(_success => {
+        console.log("Loaded Scene", sceneSwitcher.currentlyLoadedScene);
+        sceneSwitcher.currentlyLoadedScene?.asset.scale.multiplyScalar(20);
+        sceneSwitcher.currentlyLoadedScene?.asset.rotateY(Math.PI * -.5);
+
+        const orbitControls = findObjectOfType(OrbitControls);
+        if(orbitControls) orbitControls.fitCamera(scene.children, {
+            immediate: false
+        });
+        ContactShadows.auto();    
+    });
+
 })
